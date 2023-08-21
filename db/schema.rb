dc767ef012038143e8daeb310a73ec17022f94f1
Adam Lamouri <adam-lamouri@outlook.fr>
2023-08-21 13:38:21 +0200

    heroku fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_21_094658) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_21_110607) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "celebrities", force: :cascade do |t|
+    t.string "name"
+    t.integer "rating"
+    t.integer "price"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_celebrities_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +36,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_21_094658) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "celebrities", "users"
 end
