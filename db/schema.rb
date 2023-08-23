1824d08f87181b579a5fbd666eea6193f27a9d97
alice bernaud <alice.bernaud@gmail.com>
2023-08-23 17:51:45 +0200

    moyenne review

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_23_134844) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_23_154613) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,7 +55,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_23_134844) do
 
   create_table "celebrities", force: :cascade do |t|
     t.string "name"
-    t.integer "rating"
+    t.float "rating"
     t.integer "price"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
