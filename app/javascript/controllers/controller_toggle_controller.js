f978d8c48243cab43b26f6f6a6fb78f3f2d8a065
Adam Lamouri <adam-lamouri@outlook.fr>
2023-08-25 11:17:40 +0200

    connexion navbar profil

--- app/javascript/controllers/controller_toggle_controller.js
+++ app/javascript/controllers/controller_toggle_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="controller-toggle"
 export default class extends Controller {
-  static targets = ["newCelebrity", "updateCelebrity"]
+  static targets = ["newCelebrity", "updateCelebrity", "button"]
 
   connect() {
     console.log("Hello from toggle_controller.js")
@@ -11,6 +11,7 @@ export default class extends Controller {
 
   fire() {
     this.newCelebrityTarget.classList.toggle("d-none");
+    this.buttonTarget.classList.toggle("d-none")
     this.newCelebrityTarget.outerHTML = data.newCelebrity
 
   }
